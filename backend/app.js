539353eb41385b5020001afae53c6d57683c0e5d
Gangesh Yadav <gangeshyadav@Gangeshs-MacBook-Air.local>
2026-05-30 18:01:48 +0530

    Update CORS configuration

--- backend/app.js
+++ backend/app.js
@@ -6,22 +6,32 @@ import { errorMiddleware } from "./middlewares/error.js";
 import reservationRouter from "./routes/reservationRoute.js";
 
 const app = express();
+
 dotenv.config({ path: "./config.env" });
-console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
+
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST","GET"],
+    origin: [
+      process.env.FRONTEND_URL,
+      "https://online-restaurant-booking-system-xq.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/reservation", reservationRouter);
-app.get("/", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "HELLO WORLD AGAIN"
-})})
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: "HELLO WORLD AGAIN",
+  });
+});
 
 dbConnection();
 
