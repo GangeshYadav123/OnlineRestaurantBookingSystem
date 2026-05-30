82a2a708c2b5ceabbb4f13253aca58391c1f8787
Gangesh Yadav <gangeshyadav@Gangeshs-MacBook-Air.local>
2026-05-30 15:42:55 +0530

    Initial commit: Online Restaurant Booking System

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">GoBite</div>
+          <div className="right">
+            <p>Vizag</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By gangesh yadav</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By gangesh yadav.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
