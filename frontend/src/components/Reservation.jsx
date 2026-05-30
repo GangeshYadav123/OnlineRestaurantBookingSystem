273901f2a542a491dc3ee909d4859f1fd5070cbc
Gangesh Yadav <gangeshyadav@Gangeshs-MacBook-Air.local>
2026-05-30 16:59:04 +0530

    Connect frontend to Render backend

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -17,7 +17,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-  "https://onlinerestaurantbookingsystem.onrender.com",
+  "https://onlinerestaurantbookingsystem.onrender.com/api/v1/reservation/send",
   { firstName, lastName, email, phone, date, time },
   {
     headers: {
